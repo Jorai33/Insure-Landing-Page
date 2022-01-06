1e3e959dc30c05d93ce49c2fd9b3196616b4e8a6
Joraï Medjo <m.jopasi@orange.fr>
2022-01-07 00:11:36 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/globalStyle";
+import styled from "styled-components";
+
+import Logo from "./ImageComponents/Logo";
+
+const theme = {
+  colors: {
+    lightBlack: "#2C2830",
+    lightBrown: "#837D88",
+    lightViolet: "#2D2641",
+    lightBlue: "#96A9C6",
+  },
+};
+
+const MainContainer = styled.div`
+width: 100vw;
+height: 5rem;
+background-color: #FFFFFF;
+
+`
+
+const Container = styled.div`
+width: 70vw;
+margin: auto;
+border-bottom: black 3px solid;
+`;
+
+const Header = styled.header`
+  height: 5rem;
+  
+  display: flex;
+  justify-content: space-between; 
+  align-items: center; 
+`;
+
+
+const Navbar = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  gap: 0.5rem;
+  
+
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <MainContainer>
+      <Container>
+        <Header>
+          <Logo />
+          <Navbar>
+            <li>How we work</li>
+            <li>Blog</li>
+            <li>Account</li>
+            <li>
+              View Plans
+            </li>
+          </Navbar>
+        </Header>
+      </Container>
+      </MainContainer>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/globalStyle.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+ *, *::after, *::before {
+     box-sizing: border-box;
+     margin: 0; 
+     padding: 0;
+ }
+
+ body{
+     font-size: 16px;
+ }
+
+ h1 {
+    font-family: 'DM Serif Display', serif;
+ }
+
+ p {
+     font-family: "Karla", sans-serif;
+ }
+
+ li{
+     list-style: none;
+ }
+
+`;
+
+export default GlobalStyle;
